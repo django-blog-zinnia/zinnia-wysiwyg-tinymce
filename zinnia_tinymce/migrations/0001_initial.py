bbe702cd6c84d665e98d14e5772467b821466b1e
Fantomas42 <fantomas42@gmail.com>
2015-11-09 10:53:34 +0100

    Clean the initial migration

--- zinnia_tinymce/migrations/0001_initial.py
+++ zinnia_tinymce/migrations/0001_initial.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.db import models
+from django.db import migrations
 
-from django.db import models, migrations
 import zinnia_tinymce.models
 
 
@@ -14,10 +13,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='FileModel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('file_type', models.CharField(max_length=35, verbose_name='file type', choices=[(b'image', 'Image'), (b'file', 'Document')])),
-                ('uploaded_file', models.FileField(upload_to=zinnia_tinymce.models.upload_path, verbose_name='file / image')),
-                ('creation_date', models.DateTimeField(auto_now_add=True, verbose_name='creation date')),
+                ('id', models.AutoField(
+                    verbose_name='ID', serialize=False,
+                    auto_created=True, primary_key=True)),
+                ('file_type', models.CharField(
+                    max_length=35, verbose_name='file type',
+                    choices=[('image', 'Image'), ('file', 'Document')])),
+                ('uploaded_file', models.FileField(
+                    upload_to=zinnia_tinymce.models.upload_path,
+                    verbose_name='file / image')),
+                ('creation_date', models.DateTimeField(
+                    auto_now_add=True, verbose_name='creation date')),
             ],
             options={
                 'ordering': ['-creation_date'],
