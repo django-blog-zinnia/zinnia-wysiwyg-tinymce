14a8913cff699d8c79dde15365a95028063d8759
Fantomas42 <fantomas42@gmail.com>
2017-03-02 22:45:07 +0100

    Stay at version 2.2.0 for django_tinymce, for making work the project, close #6 #7

--- setup.py
+++ setup.py
@@ -32,6 +32,6 @@ setup(
     license=zinnia_tinymce.__license__,
     include_package_data=True,
     zip_safe=False,
-    install_requires=['django-tinymce',
+    install_requires=['django-tinymce<=2.2.0',
                       'sorl-thumbnail']
 )
